8d34e8649879f141c12f8fd8b6ca8d20d5f085a3
shijingliu <shijingl@seas.upenn.edu>
2014-06-13 20:37:27 -0400

    add native.java

---
+++ Native.java
@@ -0,0 +1,13 @@
+package homework4;
+
+public class Native {
+	public double InterArrivalTime; 
+	public double ArrivalTime; 
+	public double ServiceTime; 
+	public double ServiceTimeBegin; 
+	public double ServiceTimeEnd; 
+	public double TimeWaitInQueue;     
+	public boolean WaitorNot; 
+	public double TimeSpendInSystem; 
+	public double ServiceIdleTime; 
+}
